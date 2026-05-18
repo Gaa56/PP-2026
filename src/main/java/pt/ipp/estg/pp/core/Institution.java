288f00742f3f9f6f8008f622a6d8270de56874f3
Gaa56 <gabrielexmoreira@gmail.com>
2026-05-18 14:55:53 +0100

    Institution

--- src/main/java/pt/ipp/estg/pp/core/Institution.java
+++ src/main/java/pt/ipp/estg/pp/core/Institution.java
@@ -1,5 +1,38 @@
 package pt.ipp.estg.pp.core;
 
+import java.time.LocalDateTime;
+
+import pt.ipp.estg.pp.pickingManagement.PickingMap;
+import pt.ipp.estg.pp.pickingManagement.Vehicle;
+
 public interface Institution {
+
+    boolean addAidBox(AidBox aidBox);
+
+    boolean addMeasurement(Measurement measurement, Container container);
+
+    boolean addPickingMap(PickingMap pickingMap);
+
+    boolean addVehicle(Vehicle vehicle);
+
+    void disableVehicle(Vehicle vehicle);
+
+    void enableVehicle(Vehicle vehicle);
+
+    AidBox[] getAidBoxes();
+
+    Container getContainer(AidBox aidBox, ItemType itemType);
+
+    PickingMap getCurrentPickingMap();
+
+    double getDistance(AidBox aidBox);
+
+    String getName();
+
+    PickingMap[] getPickingMaps();
+
+    PickingMap[] getPickingMaps(LocalDateTime from, LocalDateTime to);
+
+    Vehicle[] getVehicles();
 
 }
