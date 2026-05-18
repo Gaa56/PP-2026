b83371ffcd39112b2553c7148c10584d0d21c5b9
David Alves <davidalves04@hotmail.com>
2026-05-18 14:20:09 +0100

    Update AidBox.java

--- src/main/java/pt/ipp/estg/pp/core/AidBox.java
+++ src/main/java/pt/ipp/estg/pp/core/AidBox.java
@@ -1,5 +1,21 @@
 package main.java.pt.ipp.estg.pp.core;
 
 public interface AidBox {
+    boolean addContainer(Container container);
 
+    String getCode();
+    
+    Container getContainer(ItemType itemType);
+
+    Container[] getContainers();
+
+    GeographicCoordinates getCoordinates();
+
+    double getDistance(AidBox aidBox);
+
+    double getDuration(AidBox aidBox);
+
+    String getRefLocal();
+
+    String getZone();
 }
