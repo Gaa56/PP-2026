e620d7e05657c7fd4c7e05bad63cc73184052c15
Gaa56 <gabrielexmoreira@gmail.com>
2026-05-18 14:47:54 +0100

    ItemType Enum e Measurement interface

--- src/main/java/pt/ipp/estg/pp/core/ItemType.java
+++ src/main/java/pt/ipp/estg/pp/core/ItemType.java
@@ -1,5 +1,20 @@
 package pt.ipp.estg.pp.core;
 
 public enum ItemType {
+    CLOTHING, MEDICINE, NON_PERISHABLE_FOOD, PERISHABLE_FOOD;
 
+    public static String itemTypeToString(ItemType itemType) {
+        switch (itemType) {
+            case CLOTHING:
+                return "Clothing";
+            case MEDICINE:
+                return "Medicine";
+            case NON_PERISHABLE_FOOD:
+                return "Non-Perishable Food";
+            case PERISHABLE_FOOD:
+                return "Perishable Food";
+            default:
+                return "Unknown";
+        }
+    }
 }
