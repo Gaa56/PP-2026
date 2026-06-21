6712d2ef9ad23b7bc3f27ed8d346925f32617158
David Alves <davidalves04@hotmail.com>
2026-06-21 14:24:52 +0100

    Update RouteValidatorImpl.java

--- src/main/java/pt/ipp/estg/pp/pickingManagement/RouteValidatorImpl.java
+++ src/main/java/pt/ipp/estg/pp/pickingManagement/RouteValidatorImpl.java
@@ -7,9 +7,11 @@ import com.estg.core.AidBox;
 public class RouteValidatorImpl implements RouteValidator {
 
     @Override
-    public boolean validate(Route arg0, AidBox arg1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'validate'");
+    public boolean validate(Route route, AidBox aidBox) {
+        if (route.containsAidBox(aidBox)) {
+            return true;
+        } else {
+            return false;
+        }
     }
-    // TODO: Implementar métodos da interface
 }
